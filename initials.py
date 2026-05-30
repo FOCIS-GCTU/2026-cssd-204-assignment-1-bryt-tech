f0a44bf378370e9d8df9d2f5845a5673435e75dc
Oklu Bright <2425404334@live.gctu.edu.gh>
2026-05-30 19:34:21 +0000

    Update initials.py

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+#File: BDO.py
+#Description: Print Out My Initials
+#Assignment Number: 1
+#
+#Name: Bright Oklu
+#Student ID: 2425404334
+#Email: 2425404334@live.gctu.edu.gh
+#Grader: Bright
+#
+#On my honor, Bright Oklu, this programming assignment is my own work
+#and I have not provided this code to any other student.
 
-def main():
-  pass #code goes here
+def main () :
+    print()
+    print("...BDO")
+    print()
 
+    print("...BBBBBBBBBB..........DDDDDDDDDD..........OOOOOOOOOOOO.....")
+    print("...BBBBBBBBBBBB........DDDDDDDDDDDD........OOOOOOOOOOOO.....")
+    print("...BBBB    BBBB........DDD     DDDD........OOOO    OOOO.....")
+    print("...BBBB    BBBB........DDD     DDDD........OOOO    OOOO.....")
+    print("...BBBBBBBBBB..........DDD     DDDD........OOOO    OOOO.....")
+    print("...BBBBBBBBBBBB........DDD     DDDD........OOOO    OOOO.....")
+    print("...BBBB    BBBB........DDD     DDDD........OOOO    OOOO.....")
+    print("...BBBB    BBBB........DDD     DDDD........OOOO    OOOO.....")
+    print("...BBBBBBBBBBBB...**...DDDDDDDDDDDD...**...OOOOOOOOOOOO...**")
+    print("...BBBBBBBBBB.....**...DDDDDDDDDD.....**...OOOOOOOOOOOO...**")
 
-
+    print()
 main()
+
+
+
+
+
+
+
